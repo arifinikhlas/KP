f01ea3b954d390e0df581b1c8f7d1b53f3aae9a6
arifinikhlas <ikhlasularifin72@gmail.com>
2022-08-02 00:41:12 +0700

    init dashboard page

--- NutrifoodSocial/NutrifoodSocial/urls.py
+++ NutrifoodSocial/NutrifoodSocial/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/dashboard'), name='home'),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
