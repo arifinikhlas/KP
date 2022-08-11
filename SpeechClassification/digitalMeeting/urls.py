ee1841fb231aac93903a261397873e06bb078575
arifinikhlas <ikhlasularifin72@gmail.com>
2022-08-11 17:22:28 +0700

    fixed digital meeting detail, search, & edit

--- SpeechClassification/digitalMeeting/urls.py
+++ SpeechClassification/digitalMeeting/urls.py
@@ -6,5 +6,8 @@ app_name = 'digitalMeeting'
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('<int:id>/edit/', views.edit, name='edit'),
+    path('<int:id>/savedit/', views.savedit, name='savedit'),
+    path('<int:id>/detail/', views.detail, name='detail'),
+    path('search/', views.search, name='search'),
 ]
--- SpeechClassification/digitalMeeting/views.py
+++ SpeechClassification/digitalMeeting/views.py
@@ -7,6 +7,43 @@ def index(request):
 
     meeting_list = Meeting.objects.all()
 
-    context = {'meeting_list': meeting_list}
+    context = {'meeting_list': meeting_list, 'keyword':''}
 
     return render(request, 'digitalMeeting/index.html', context)
+
+def edit(request, id):
+    meeting = get_object_or_404(Meeting, id=id)
+
+    context = {'meeting': meeting}
+    
+    return render(request, 'digitalMeeting/edit.html', context)
+
+def savedit(request, id):
+    meeting = get_object_or_404(Meeting, id=id)
+
+    meeting.title = request.POST['title']
+    meeting.description = request.POST['description']
+    meeting.thumbnail = request.POST['thumbnail']
+    meeting.pub_date = timezone.now()
+    # file.location = request.POST['location']
+    meeting.save()
+
+    return redirect('/digitalMeeting')
+
+def detail(request, id):
+    meeting = get_object_or_404(Meeting, id=id)
+
+    context = {'meeting': meeting}
+    
+    return render(request, 'digitalMeeting/detail.html', context)
+
+def search(request):
+    list = Meeting.objects.all()
+    keyword = ''
+    if(request.method=='POST'):
+        keyword = request.POST.get('keyword')
+        list = Meeting.objects.filter(title__icontains=keyword)   
+
+    context = {'meeting_list' : list, 'keyword' : keyword}
+
+    return render(request,'digitalMeeting/index.html', context)
